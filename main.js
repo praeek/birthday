f1b40a5d396427be1da1e08aea36dd56876bb12b
praeek <105891580+praeek@users.noreply.github.com>
2023-01-10 20:32:49 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var canvas = new fabric.Canvas('myCanvas');
+var x= document.getElementById("myAudio");
+
+function new_image()
+{
+fabric.Image.fromURL('BirthdayImage.jpg', FUNCTION(Img){
+block_object
+});
+
+
+
+
+	
+
+
+}
+
+function playSound(){
+
+}
